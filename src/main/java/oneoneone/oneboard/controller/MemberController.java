adc88eeba30b4aa52e6aa3bb9d34cb9a54cdff7e
daehosong <thdeogh136@gmail.com>
2023-07-26 02:12:10 +0900

    Revert "07.25"
    This reverts commit c91f17f11006f65b2e3fa67a526d11e73ec644ab.

--- src/main/java/oneoneone/oneboard/controller/MemberController.java
+++ src/main/java/oneoneone/oneboard/controller/MemberController.java
@@ -5,7 +5,10 @@ import oneoneone.oneboard.dto.MemberDTO;
 import oneoneone.oneboard.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 import java.lang.reflect.Member;
@@ -54,14 +57,12 @@ public class MemberController {
         model.addAttribute("memberList",memberDTOList);
         return "list";
     }
-
     @GetMapping("/member/{id}")
     public String findById(@PathVariable Long id,Model model){
         MemberDTO memberDTO = memberService.findById(id);
         model.addAttribute("member",memberDTO);
         return "memberView";
     }
-
     @GetMapping("/member/update")
     public String memberUpdateForm(HttpSession session,Model model){
         String myEmail = (String)session.getAttribute("loginEmail");
@@ -69,37 +70,10 @@ public class MemberController {
         model.addAttribute("updateMember",memberDTO);
         return "update";
     }
-
     @PostMapping("/member/update")
     public String update(@ModelAttribute MemberDTO memberDTO){
         memberService.update(memberDTO);
         return "redirect:/member/"+memberDTO.getId();
-    }
-
-    @GetMapping("/member/delete/{id}")
-    public String deleteById(@PathVariable Long id){
-        memberService.deleteById(id);
-        return "redirect:/member/";
-    }
-
-    @GetMapping("/member/logout")
-    public String memberLogout( HttpSession session){
-        //  invalidate()  무효화 , 상쇄
-        session.invalidate();
-        return "index";
-    }
-
-    @ResponseBody
-    @PostMapping("/member/email-check")
-    public String emailCheck(@RequestParam("memberEmail") String memberEmail){
-        System.out.println("memberEmail : "+memberEmail);
-        String checkResult=memberService.emailCheck(memberEmail);
-        if(checkResult!=null){
-            return "ok";
-        }
-        else {
-            return null;
-        }
     }
 
 }
